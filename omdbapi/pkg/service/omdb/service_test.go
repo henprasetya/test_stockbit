168e70e85cc5955039169a6da91c0e2928cd5389
Henda Prasetya <prasetya.henda@gmail.com>
2021-11-22 13:50:29 +0700

    update service

--- omdbapi/pkg/service/omdb/service_test.go
+++ omdbapi/pkg/service/omdb/service_test.go
@@ -64,3 +64,56 @@ func Test_service_SearchMovie(t *testing.T) {
 		})
 	}
 }
+
+func Test_service_MovieDetail(t *testing.T) {
+	mockCtrl := gomock.NewController(t)
+	mockCtrl.Finish()
+
+	mockApi := mockOmdb.NewMockMovieRepo(mockCtrl)
+
+	type fields struct {
+		api omdb.MovieRepo
+	}
+	type args struct {
+		omdbID string
+	}
+	tests := []struct {
+		name    string
+		fields  fields
+		args    args
+		want    *model.Movie
+		wantErr bool
+		mock func()
+	}{
+		{
+			name:    "error movie detail",
+			fields:  fields{
+				api: mockApi,
+			},
+			args:    args{
+				omdbID: "1",
+			},
+			want:    nil,
+			wantErr: true,
+			mock: func() {
+				mockApi.EXPECT().MovieDetail(gomock.Any(), gomock.Any()).Return(nil, errors.New("error")).Times(1)
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			s := &service{
+				api: tt.fields.api,
+			}
+			tt.mock()
+			got, err := s.SearchMovie(tt.args.search, tt.args.page)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("SearchMovie() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("SearchMovie() got = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
